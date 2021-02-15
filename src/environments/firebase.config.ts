9ac86f4f29d09ffc9161fd5c03ef727ee47eaf1b
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-15 10:41:20 -0300

    criação do arquivo firebase.config.ts arquivo de configuração de banco de dados

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,10 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyDuVeDaOM9vALGi4xN7EAmJu1n8NbAeWgw",
+  authDomain: "whatsappclonemastersix.firebaseapp.com",
+  databaseURL: "https://whatsappclonemastersix.firebaseio.com",
+  projectId: "whatsappclonemastersix",
+  storageBucket: "whatsappclonemastersix.appspot.com",
+  messagingSenderId: "265712287603",
+  appId: "1:265712287603:web:a88255b33579b96fe48f06",
+  measurementId: "G-KFZB1CGDY6"
+};
