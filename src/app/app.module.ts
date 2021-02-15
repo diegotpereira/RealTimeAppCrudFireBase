dad49f5c5199f0a7c05c6ce98383721a87af229a
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-15 10:40:22 -0300

    alteração no arquivo de banco de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroPessoaComponent } from './cadastro-pessoa/cadastro-pessoa.component';
+import { FirebaseConfig } from './../environments/firebase.config';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { CadastroPessoaModule } from './cadastro-pessoa/cadastro-pessoa.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroPessoaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule, CadastroPessoaModule,
+    AngularFireModule.initializeApp(FirebaseConfig),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
